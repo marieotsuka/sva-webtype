2ae584d91cca540069ed90a8a0ac44df6ac368bd
Marie Otsuka <motsuka.gd@gmail.com>
2024-11-10 18:34:50 -0500

    [interactive] add JS toggles

---
+++ js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	const body = document.body;
+	let sticker = true;
+
+	document.getElementById('option-simplified').addEventListener('change', (e)=>{		
+		if (e.target.checked){
+			body.style.setProperty('--ss01', 1);
+			body.style.setProperty('--ss03', 1);
+		}else{
+			body.style.setProperty('--ss01', 0);
+			body.style.setProperty('--ss03', 0);
+		}
+	});
+
+
+
+	const stickerEl = document.getElementById('tag-label');
+	document.getElementById('option-sticker').addEventListener('change', (e)=>{		
+		if (e.target.checked){
+			sticker = true;			
+		}else{
+			sticker = false;
+			stickerEl.style.cssText = 'display: none;';	
+		}
+	});
+	
+  body.addEventListener('click', function(e){  	
+  	if( sticker ){
+  		let target = e.target;
+  		var elementname = e.target.tagName;
+  		console.log(elementname);
+
+  		if( elementname !== 'summary' && target.closest('a') == null ){
+  			// if not clicking on link
+  			var x = e.pageX;
+  			var y = e.pageY;
+  			stickerEl.querySelector('.tag-name').innerText = elementname;
+  			stickerEl.style.cssText = `display: block; top: ${y-30}px; left: ${x+30}px;`;
+  		}
+  	}else{
+  		stickerEl.style.cssText = 'display: none;';
+  	}
+  });
+
+
+});
